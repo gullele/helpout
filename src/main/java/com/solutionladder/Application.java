3b501c67be72a4273da2ea414ec6f945f3a5c605
Kaleb <kaleb@gullele.com>
2017-09-15 07:02:06 -0400

    Making the app deployable to tomcat

--- src/main/java/com/solutionladder/Application.java
+++ src/main/java/com/solutionladder/Application.java
@@ -1,19 +1,31 @@
 package com.solutionladder;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Main Spring boot class that will be triggered first.
+ * This class has to extend from SpringBootServletInitializer to make it deployable to tomcat 
+ * Also, in build.gradle, the providedRuntime("org.springframework.boot:spring-boot-starter-tomcat") should be added
+ *
+ * @author Kaleb Woldearegay <kaleb@solutionladder.com>
+ */
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
-    public static void main(String[] args) {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
 }
